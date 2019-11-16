c16d08c51ebc7f4ea8c6462961913bc78944b60f
AislamEldesoky <islameldesoky@hotmail.com>
2019-11-16 03:58:38 +0300

    a little change

--- app/src/androidTest/java/com/example/premierleague/KakaoScreen.kt
+++ app/src/androidTest/java/com/example/premierleague/KakaoScreen.kt
@@ -7,7 +7,7 @@ import com.agoda.kakao.Screen
 class KakaoScreen:Screen<KakaoScreen>(){
 
     val nameTextView : KTextView =
-        KTextView{withId(R.id.teamname)}
+        KTextView{withId(R.id.name)}
     val webTextView : KTextView =
         KTextView{withId(R.id.website)}
     val colorTextView : KTextView =
